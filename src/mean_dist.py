73de16a5391f734a4e6c2d71bfb9d6844bf109ed
oxygen-TW <howard2805@gmail.com>
2023-09-29 00:28:51 +0800

    [ADD] Add MeanDist() to get the mean value of k nearest neighbors and update DataFrame.

--- src/mean_dist.py
+++ src/mean_dist.py
@@ -1,39 +1,68 @@
-from .n_facilities import NFacilities
+import pandas as pd
+from shapely.geometry import Point
+from .utils import load_data, add_twd97_coordinates_to_dataframe as add_twd97
 
-class MeanDist(NFacilities):
+class MeanDist():
     """
     The class that can get the mean value of k nearest neighbors
     """
     def __init__(self, facility_path: str, target_path: str):
-        #This class does not need the radius, so I set it to 0
-        #Override functions that need the radius to prevent errors
-        super().__init__(facility_path, target_path, 0)
+        self.facility = load_data(facility_path)
+        self.target = load_data(target_path)
+        self.facilities_with_dist, self.target_pos = self.split_data()
 
-    def mean(self, k=3):
+    def split_data(self) -> tuple:
         """
-        Get the mean value of k nearest neighbors
+        Get the (x, y) for facility and target
         """
-        facility_pos, target_pos = self.split_data()
+        facility_pos = add_twd97(self.facility)[['橫坐標', '縱坐標']]
+        target_pos = self.target[['橫坐標', '縱坐標']]
+        return facility_pos, target_pos
 
-        #sort by distance
-        facility_pos = self.calculate_dist(facility_pos, target_pos)
-        facility_pos = facility_pos.sort_values(by = 'distance', ascending=True)
+    def calc_nn_mean_dist(self,x:int, y:int, k=3)->float:
+        """
+        Get the mean value of point to k nearest neighbors
+        """
+        ref_point = Point(x, y)
+
+        #Get the distance between each facility and the target
+        self.facilities_with_dist['distance'] = self.facilities_with_dist.apply(
+            lambda row: ref_point.distance(
+                Point(row['橫坐標'], row['縱坐標'])
+            ),
+            axis = 1
+        )
+
+        #Sort the dataframe by distance
+        self.facilities_with_dist = self.facilities_with_dist.sort_values(
+            by = 'distance', 
+            ascending=True
+        )
 
         #return mean value of k nearest neighbors
-        mean = facility_pos.iloc[0:k, 2].mean()
+        mean = self.facilities_with_dist.iloc[0:k, 2].mean()
         return mean
 
-    #Override the function that can not be used in this class
-    def find_n_facilities(self):
-        raise NotImplementedError("find_n_facilities can not use in MeanDist")
+    def update_dataframe(self, column_name="nn_mean_distance") -> pd.DataFrame:
+        """
+        Update the dataframe
+        """
+        # Apply the mean function using lambda and assign the result to a new column in self.target
+        self.target[column_name] = self.target_pos.apply(self.__mean_function, axis=1)
 
-    def main(self):
-        raise NotImplementedError("find_n_facilities can not use in MeanDist")
+        return self.target
+
+    def __mean_function(self, row):
+        """
+        Private function for calculate mean
+        """
+        return self.calc_nn_mean_dist(row.iloc[0], row.iloc[1])
 
 if __name__ == "__main__":
     TARGET_DATA = "data/small_training_data.csv"
     FACILITY_DATA = "data/external_data/金融機構基本資料.csv"
+    OUTPUT_DATA = "data/output.csv"
 
     md = MeanDist(FACILITY_DATA, TARGET_DATA)
-    md.main()
-    print(md.mean(k=5))
+
+    md.update_dataframe().to_csv(OUTPUT_DATA, index=False)
